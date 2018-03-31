21d8094cfeccd53184fb9d482aaf599606f6cc0d
sungbin <21700613@handong.edu>
2018-03-31 23:50:19 +0900

    prob2 Ckicken class have skeleton code

---
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -0,0 +1,35 @@
+package edu.handong.csee.java.lab07.prob2;
+
+public class Chicken {
+	public String name;
+	public double price;
+	public int stars;
+	
+	public void Set_name(String name)
+	{
+		this.name = name;
+	}
+	public void Set_price(double price)
+	{
+		this.price = price;
+	}
+	public void Set_stars(int stars)
+	{
+		this.stars = stars;
+	}
+	public String Get_name()
+	{
+		return this.name;
+	}
+	public double Get_price()
+	{
+		return this.price;
+	}
+	public int Get_stars()
+	{
+		return this.stars;
+	}
+	
+	
+
+}
