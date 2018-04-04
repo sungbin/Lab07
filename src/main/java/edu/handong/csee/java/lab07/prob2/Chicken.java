e5e26da97d19b4c8a846171d1ba246e06ae43a29
sungbin <21700613@handong.edu>
2018-04-04 19:35:08 +0900

    add javadoc and single reference in Chicken, main class

--- src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -1,51 +1,51 @@
-package edu.handong.csee.java.lab07.prob2;
+package edu.handong.csee.java.lab07.prob2; //The file is in this package
 
-public class Chicken {
-	public String name;
-	public double price;
-	public int stars;
-	
+/**
+ * This class have 3 variable (name, price, stars)
+ * Set_functions help input value to variables.
+ * Get_functions help output value of variables.
+ * making instance, get or set variable value.
+ */
+public class Chicken { //declare Chicken class
+	public String name; //declare name variable type public String
+	public double price; //declare price variable public double type
+	public int stars; //declare stars variable public int type
 
-
-	public Chicken(String name, double price, int stars) {
-		this.name = name;
-		this.price = price;
-		this.stars = stars;
-	}
-
-	public Chicken()
-	{
-	this.name = "";
-	this.price = 0.0;
-	this.stars = 0;
+	public Chicken(String name, double price, int stars) { //making instance, this function get variable value
+		this.name = name; //input value to name class variable
+		this.price = price; //input value to price class variable
+		this.stars = stars; //input value to stars class variable
 	}
 
-	
-	public void Set_name(String name)
+	public Chicken() //making instance, this function set variable value.
 	{
-		this.name = name;
-	}
-	public void Set_price(double price)
-	{
-		this.price = price;
-	}
-	public void Set_stars(int stars)
-	{
-		this.stars = stars;
-	}
-	public String Get_name()
-	{
-		return this.name;
-	}
-	public double Get_price()
-	{
-		return this.price;
-	}
-	public int Get_stars()
-	{
-		return this.stars;
+	this.name = ""; //set class variable, name
+	this.price = 0.0; //set class variable, price
+	this.stars = 0; //set class variable, stars
 	}
 	
-	
-
+	public void Set_name(String name) //declare Set_name function public type that input value from user to class variable
+	{
+		this.name = name; //input the value to name class variable
+	}
+	public void Set_price(double price) //declare function public type that input value from user to class variable
+	{
+		this.price = price; //input the value to price class variable
+	}
+	public void Set_stars(int stars) //declare function public type that input value from user to class variable
+	{
+		this.stars = stars; //input the value to stars class variable
+	}
+	public String Get_name() //declare Get_name function, return type is String
+	{
+		return this.name; //return the value
+	}
+	public double Get_price() //declare Get_price function, return type is double
+	{
+		return this.price; //return the value
+	}
+	public int Get_stars() //declare Get_stars function, return type is int
+	{
+		return this.stars; //return the value
+	}
 }
--- src/main/java/edu/handong/csee/java/lab07/prob2/main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/main.java
@@ -1,20 +1,24 @@
-package edu.handong.csee.java.lab07.prob2;
+package edu.handong.csee.java.lab07.prob2; //The file is in this package
 
-public class main {
+/**
+ * main function have 3 instance of chicken class.
+ * to operate well, need Chicken functions including setting and saving.
+ */
+public class main { //declare main class public type
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //declare main function
 		// TODO Auto-generated method stub
-		Chicken menu1 = new Chicken("Cheese_mustard",17000.00,5);
-		Chicken menu2 = new Chicken("Honey_mustard",17000.00,5);
-		Chicken menu3 = new Chicken("Spicey_chicken",17000.00,5);
+		Chicken menu1 = new Chicken("Cheese_mustard",17000.00,5); //make instance of Chicken
+		Chicken menu2 = new Chicken("Honey_mustard",17000.00,5); //make instance of Chicken
+		Chicken menu3 = new Chicken("Spicey_chicken",17000.00,5); //make instance of Chicken
 		
-		menu1.Set_stars(3);
-		menu2.Set_stars(4);
-		menu3.Set_stars(1);
+		menu1.Set_stars(3); //input the value to stars variable in Chicken instance
+		menu2.Set_stars(4); //input the value to stars variable in Chicken instance
+		menu3.Set_stars(1); //input the value to stars variable in Chicken instance
 		
-		System.out.println(menu1.name + "'s rating is " + menu1.stars);
-		System.out.println(menu2.name + "'s rating is " + menu2.stars);
-		System.out.println(menu3.name + "'s rating is " + menu3.stars);
+		System.out.println(menu1.name + "'s rating is " + menu1.stars); //print stars variable in menu1 instance
+		System.out.println(menu2.name + "'s rating is " + menu2.stars); //print stars variable in menu2 instance
+		System.out.println(menu3.name + "'s rating is " + menu3.stars); //print stars variable in menu3 instance
 
 	}
 
