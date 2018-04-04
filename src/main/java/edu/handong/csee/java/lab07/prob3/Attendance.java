8767e479a13fc06a15a4609a0e9a677b7a44baa0
sungbin <21700613@handong.edu>
2018-04-04 20:44:45 +0900

    add javadoc and single line reference at Attendance

--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
@@ -1,55 +1,61 @@
-package edu.handong.csee.java.lab07.prob3;
+package edu.handong.csee.java.lab07.prob3; //The file is in this package
 
-public class Attendance {
-	String name;
-	int grade;
-	int id;
-	int missed;
-	public Attendance(String name, int grade, int id, int missed)
+/**
+ * this class have 3 variable (name, grade,id,missed)
+ * can set or reset the variables.
+ * for each variable, there are Getter and Setter method
+ * can print result if student is failed or not.
+ */
+public class Attendance { //declare Attendance public type
+	String name; //declare class variable String type
+	int grade; //declare class variable integer type
+	int id; //declare class variable integer type
+	int missed; //declare class variable integer type
+	public Attendance(String name, int grade, int id, int missed) //method help set variables.
 	{
-		this.name = name;
-		this.grade = grade;
-		this.id = id;
-		this.missed = missed;
+		this.name = name; //put value into class name variable 
+		this.grade = grade; //put value into class grade variable 
+		this.id = id; //put value into class id variable 
+		this.missed = missed; //put value into class missed variable 
 	}
-	public Attendance()
+	public Attendance() //method help initializing variables.
 	{
-		this.name = "";
-		this.grade = 0;
-		this.id = 0;
-		this.missed =0;
+		this.name = ""; //initializing name
+		this.grade = 0; //initializing grade
+		this.id = 0; //initializing id
+		this.missed =0; //initializing missed
 	}
-	public String GetName() {
-		return name;
+	public String GetName() { //getter method of name
+		return name; //return value of name
 	}
-	public void SetName(String name) {
-		this.name = name;
+	public void SetName(String name) { //setter method of name
+		this.name = name; //set name
 	}
-	public int GetGrade() {
-		return grade;
+	public int GetGrade() { //getter method of name
+		return grade; //return value of grade
 	}
-	public void SetGrade(int grade) {
-		this.grade = grade;
+	public void SetGrade(int grade) { //setter method of name
+		this.grade = grade; //set grade
 	}
-	public int GetId() {
-		return id;
+	public int GetId() { //getter method of name
+		return id; //return value of id
 	}
-	public void SetId(int id) {
-		this.id = id;
+	public void SetId(int id) { //setter method of name
+		this.id = id; //set id
 	}
-	public int GetMissed() {
-		return missed;
+	public int GetMissed() { //getter method of name
+		return missed; //return value of missed
 	}
-	public void SetMissed(int missed) {
-		this.missed = missed;
+	public void SetMissed(int missed) { //setter method of name
+		this.missed = missed; //set missed
 	}
 	
-	public void Print_result()
+	public void Print_result() //method printing result
 	{
-		if(this.missed>6)
-			System.out.println("I'm sorry, " + this.name +". You failed this course. the number of missed class is " + this.missed);
-		else
-			System.out.println(this.name + ", the number of missed class is " + this.missed + ". we will see about the grade.");
+		if(this.missed>6) //if missed class variable is bigger than six,
+			System.out.println("I'm sorry, " + this.name +". You failed this course. the number of missed class is " + this.missed); //print this sentence
+		else //or not,
+			System.out.println(this.name + ", the number of missed class is " + this.missed + ". we will see about the grade."); //print this sentence
 	}
 
 }
